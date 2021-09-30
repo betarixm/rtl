9a8d86728c2fc71f1b14db1651230004c7315817
beta <beta.underalpha@gmail.com>
2021-09-30 18:56:56 +0900

    🧑‍💻 Models

--- core/client/models.py
+++ core/client/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.utils.timezone import now
+from django.utils.translation import gettext_lazy as _
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+def validate_student_id(value):
+    try:
+        int(value)
+    except ValueError:
+        raise ValidationError(_("학번을 올바른 형식으로 입력해주세요."))
+
+    if len(value) != 8:
+        raise ValidationError(_("학번을 올바른 형식으로 입력해주세요."))
+
+    if Client.objects.filter(id=value).first() is not None:
+        raise ValidationError(_("해당 학번은 이미 등록되어 있습니다."))
+
+
+class Client(models.Model):
+
+    id = models.CharField(
+        "학번",
+        max_length=8,
+        primary_key=True,
+        null=False,
+        blank=False,
+        validators=[validate_student_id],
+    )
+    name = models.CharField("이름", max_length=1000, null=False, blank=False)
+    phone_number = PhoneNumberField("전화번호", null=False, blank=False)
+    last_access_at = models.DateTimeField(
+        "마지막 접속 시간", default=now, null=False, blank=False
+    )
+
+    def clean(self):
+        validate_student_id(self.id)
+
+    class Meta:
+        verbose_name = "클라이언트"
+        verbose_name_plural = "클라이언트들"
+
+    def __str__(self):
+        return self.id
--- core/event/models.py
+++ core/event/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+import uuid
+
+
+class Event(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField("제목", max_length=100)
+
+    class Meta:
+        verbose_name = "이벤트"
+        verbose_name_plural = "이벤트들"
+
+    def __str__(self):
+        return self.title
+
