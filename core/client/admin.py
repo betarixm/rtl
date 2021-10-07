75bdde87a8b21de0cfd081ba9bdcf38c149df09d
beta <beta.underalpha@gmail.com>
2021-10-08 00:34:47 +0900

    👷 Add admin

--- core/client/admin.py
+++ core/client/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Client, Ticket
 
-# Register your models here.
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    pass
--- core/event/admin.py
+++ core/event/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Event
 
-# Register your models here.
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    def id_hex(self, obj: Event):
+        return obj.id.hex
+
+    readonly_fields = ("id_hex", )
