6d51c192560738a5ecb54a7d697b2a30e5e31fa3
beta <beta.underalpha@gmail.com>
2022-04-26 17:00:57 +0900

    🎨 Change button color

--- app/src/pages/Event.tsx
+++ app/src/pages/Event.tsx
@@ -116,7 +116,7 @@ class _Event extends React.Component<EventProps, EventState> {
                             <button data-modal-toggle="popup-modal" type="button" onClick={() => {
                                 this.setState({isDraw: false})
                             }}
-                                    className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
+                                    className="text-white bg-gradient-to-r from-postech-400 to-red-500 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
                                 Close
                             </button>
                         </div>
